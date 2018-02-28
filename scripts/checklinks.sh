94e3bced3e484c8cd275a04843317e50cd37e8cc
Gaurav Nelson <gnelson@redhat.com>
2018-02-28 21:06:52 +1000

    script fixes

--- scripts/checklinks.sh
+++ scripts/checklinks.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 set -ev
 
-git diff --name-only HEAD...$TRAVIS_BRANCH
+#git diff --name-only HEAD...$TRAVIS_BRANCH
 
-asciidoc-link-check -p creating_images/custom.adoc
-
-#if [ "${TRAVIS_PULL_REQUEST}" = "true" ]; then
+#asciidoc-link-check -p creating_images/custom.adoc
+echo "${TRAVIS_PULL_REQUEST}"
+COMMIT_HASH="$(git log -n 1 --skip 1 --pretty=format:"%H")" #get the second last commit hash because Travis merges PR and creates a new commit
+if [ "${TRAVIS_PULL_REQUEST}" != "false" ]; then
   echo -e "\e[32mHYPERLINKS CHECK\e[0m"
   #run the loop for every modified or added file
-  for i in $(git diff --name-only --diff-filter=AM HEAD...$TRAVIS_BRANCH) ; do
-
+  for i in $(git diff --name-only "${COMMIT_HASH}") ; do
       fileList[$N]="$i"
-      echo "$(asciidoc-link-check $i -p)" #check all hyperlinks in this file
+      echo "$(asciidoc-link-check $i)" #check all hyperlinks in this file
       echo $'\n'
       let "N= $N + 1"
   done
-#fi
+fi
 echo -e "\e[32mDONE!\e[0m"
