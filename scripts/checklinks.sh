702845ba955d0c08ea54da6aa1514c4a580957d2
Gaurav Nelson <gnelson@redhat.com>
2018-02-28 15:45:42 +1000

    updates to script

--- scripts/checklinks.sh
+++ scripts/checklinks.sh
@@ -2,10 +2,8 @@
 set -ev
 #if [ "${TRAVIS_PULL_REQUEST}" = "true" ]; then
   echo -e "\e[32mHYPERLINKS CHECK\e[0m"
-  COMMIT_HASH="$(git rev-parse @~)" #get the previous commit hash
-
   #run the loop for every modified or added file
-  for i in $(git diff --name-only "${COMMIT_HASH}") ; do
+  for i in $(git diff --name-only --diff-filter=AM HEAD...$TRAVIS_BRANCH) ; do
 
       fileList[$N]="$i"
       echo "$(asciidoc-link-check $i -p)" #check all hyperlinks in this file
