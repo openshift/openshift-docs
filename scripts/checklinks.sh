07479bf064762a5a6a0de39689a87aa983d8074a
Gaurav Nelson <gnelson@redhat.com>
2018-02-28 21:14:26 +1000

    lets try trvis sha

--- scripts/checklinks.sh
+++ scripts/checklinks.sh
@@ -5,11 +5,11 @@ set -ev
 
 #asciidoc-link-check -p creating_images/custom.adoc
 echo "${TRAVIS_PULL_REQUEST}"
-COMMIT_HASH="$(git log -n 1 --skip 1 --pretty=format:"%H")" #get the second last commit hash because Travis merges PR and creates a new commit
+#COMMIT_HASH="$(git log -n 1 --skip 1 --pretty=format:"%H")" #get the second last commit hash because Travis merges PR and creates a new commit
 if [ "${TRAVIS_PULL_REQUEST}" != "false" ]; then
   echo -e "\e[32mHYPERLINKS CHECK\e[0m"
   #run the loop for every modified or added file
-  for i in $(git diff --name-only "${COMMIT_HASH}") ; do
+  for i in $(git diff --name-only "${TRAVIS_PULL_REQUEST_SHA}") ; do
       fileList[$N]="$i"
       echo "$(asciidoc-link-check $i)" #check all hyperlinks in this file
       echo $'\n'
