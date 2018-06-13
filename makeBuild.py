56a6758115cb6101e2c72f5843b4209b61255e0f
Vikram Goyal <vigoyal@redhat.com>
2018-06-13 14:15:35 +1000

    dfd

--- makeBuild.py
+++ makeBuild.py
@@ -1,1 +1,26 @@
-build.py --distro openshift-enterprise --product "OpenShift Container Platform" --version 3 --no-upstream-fetch
+import sys
+
+from lxml.etree import XMLSyntaxError, XIncludeError
+
+from aura import utils
+from aura.transformers import AsciiDocPublicanTransformer
+
+# Create the transformer instance
+transformer = AsciiDocPublicanTransformer()
+
+# Transform the AsciiDoc to DocBook XML
+transformer._build_docbook_src("master.adoc", "build")
+transformer._before_xml_parse("build/master.xml")
+
+# Parse the transformed XML
+try:
+    # Parse the XML content
+    tree = utils.parse_xml(asciidoc_xml_file)
+except (XMLSyntaxError, XIncludeError) as e:
+    print("Validation failed")
+    sys.exit(-1)
+
+# Validate the transformed XML
+if not transformer._validate_docbook_xml_basic("build", tree):
+    print("Validation failed")
+    sys.exit(-1)
