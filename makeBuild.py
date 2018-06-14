1cb2361b4a681c02e20ef1751fa58c3ea47b4d4c
Vikram Goyal <vigoyal@redhat.com>
2018-06-14 13:03:13 +1000

    Fixes

--- makeBuild.py
+++ makeBuild.py
@@ -2,39 +2,49 @@ import sys
 import os
 import logging
 
-logging.basicConfig(level=logging.INFO, format="%(levelname)s %(message)s")
-
-print(os.getcwd() + "\n")
-os.chdir("drupal-build/install_config/")
-#print(os.getcwd() + "\n")
-
 from lxml.etree import XMLSyntaxError, XIncludeError
 
-from aura import utils
+from aura import cli, utils
 from aura.transformers.tf_asciidoc import AsciiDocPublicanTransformer
 
-# Create the transformer instance
-transformer = AsciiDocPublicanTransformer()
+# list of books - CHANGE HERE
+book_list = ['install_config', 'architecture', 'getting_started', 'admin_guide', 'dev_guide']
 
-logging.Logger.verbose = logging.Logger.debug
-# Transform the AsciiDoc to DocBook XML
-#os.chdir("drupal-build/install_config/")
-transformer._build_docbook_src("master.adoc", "build")
-transformer._before_xml_parse("build/master.xml")
+# Initialize logging
+cli.init_logging(False, False)
 
-# print ''.join(file('build/master.xml'))
+for book in book_list:
 
-# Parse the transformed XML
-try:
+  #print(os.getcwd() + "\n")
+  os.chdir("drupal-build/" + book)
+  #print(os.getcwd() + "\n")
+
+  # Create the transformer instance
+  transformer = AsciiDocPublicanTransformer()
+
+  # Transform the AsciiDoc to DocBook XML
+  if not transformer._build_docbook_src("master.adoc", "build"):
+    sys.exit(-1)
+  transformer._before_xml_parse("build/master.xml")
+
+  # Parse the transformed XML
+  try:
     # Parse the XML content
     tree = utils.parse_xml("build/master.xml")
-except (XMLSyntaxError, XIncludeError) as e:
+    
+    transformer._fix_uncoverted_xrefs_with_file_paths(tree)
+
+    # Validate the transformed XML
+    if not transformer._validate_docbook_idrefs(tree):
+      print("Validation failed")
+      sys.exit(-1)
+    
+    os.chdir("../../")
+    
+  except (XMLSyntaxError, XIncludeError) as e:
     logging.error(e)
     logging.error("Unable to parse the AsciiDoc built DocBook XML")
     print("Validation failed")
-    sys.exit(-1)
+    os.chdir("../../")
 
-# Validate the transformed XML
-if not transformer._validate_docbook_xml_basic("build", tree):
-    print("Validation failed")
-    sys.exit(-1)
+    #sys.exit(-1)
