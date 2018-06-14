54497c3320ff684bcce9e36a57d56e1c17b16a49
Vikram Goyal <vigoyal@redhat.com>
2018-06-15 08:57:48 +1000

    test

--- makeBuild.py
+++ makeBuild.py
@@ -8,9 +8,9 @@ from aura import cli, utils
 from aura.exceptions import InvalidInputException
 from aura.transformers.tf_asciidoc import AsciiDocPublicanTransformer
 
-branch = os.system("git symbolic-ref -q --short HEAD")
+#branch = os.system("git symbolic-ref -q --short HEAD")
 
-print(branch)
+#print(branch)
 
 # list of books - CHANGE HERE
 book_list = ['admin_guide', 'apb_devel', 'architecture', 
