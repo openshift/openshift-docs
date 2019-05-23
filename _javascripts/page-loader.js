c33a22fc7fbf2fcfa5b2dd1029973e1cb470ea5c
Vikram Goyal <vigoyal@redhat.com>
2019-05-23 16:11:47 +1000

    adding tool to load modules

--- _javascripts/page-loader.js
+++ _javascripts/page-loader.js
@@ -51,4 +51,20 @@ function versionSelector(list) {
 function selectVersion(currentVersion) {
   document.getElementById("version-selector").value = currentVersion;
   // alert(currentVersion);
+
+  // in enterprise branch 4, we have modules and this is an attempt to load the
+  // modules by double clicking on them.
+  if(currentVersion.startswith("4")) {
+    var element = document.getElementsByTagName('h2');
+    Object.entries(element).map(( object ) => {
+      object[1].addEventListener("dblclick", function() {
+        // alert(this.id);
+        // alert(this.id.split("_", 1)[0] + ".adoc");
+        var fn = this.id.split("_", 1)[0] + ".adoc";
+        window.open("https://github.com/openshift/openshift-docs/tree/enterprise-" +
+          currentVersion + "/modules/" + fn, "_new");
+      });
+    });
+  }
+
 }
