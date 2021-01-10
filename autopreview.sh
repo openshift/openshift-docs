6f32e0ed4f562c263a1ebea36ace44bdb7eea515
Vikram Goyal <vigoyal@redhat.com>
2021-01-10 10:18:58 +1000

    Update autopreview.sh

--- autopreview.sh
+++ autopreview.sh
@@ -2,7 +2,7 @@
 set -ev
 
 ALLOWED_USERS=("aireilly" "mburke5678" "vikram-redhat" "abrennan89" "ahardin-rh" "kalexand-rh" "adellape" "bmcelvee" "ousleyp" "lamek" "JStickler" "rh-max" "bergerhoffer" "sheriff-rh" "jboxman" "bobfuru" "aburdenthehand" "boczkowska" "Preeticp" "neal-timpe" "codyhoag" "apinnick" "bgaydosrh" "lmandavi" "maxwelldb" "pneedle-rh" "lbarbeevargas" "jeana-redhat" "RichardHoch" "johnwilkins" "sjhala-ccs" "mgarrellRH" "SNiemann15" "sfortner-RH" "jonquilwilliams" "ktania46" "wking" "
-jc-berger" "rishumehra" "iranzo" "abhatt-rh")
+jc-berger" "rishumehra" "iranzo" "abhatt-rh" "@mohit-sheth")
 USERNAME=${TRAVIS_PULL_REQUEST_SLUG::-15}
 COMMIT_HASH="$(git rev-parse @~)"
 mapfile -t FILES_CHANGED < <(git diff --name-only "$COMMIT_HASH")
