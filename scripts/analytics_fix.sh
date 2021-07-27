2e2d34e87466562581ab2fc2b5ec6c7bf02ee8ff
Vikram Goyal <vigoyal@redhat.com>
2021-07-27 10:49:37 +1000

    fix script links

--- scripts/analytics_fix.sh
+++ scripts/analytics_fix.sh
@@ -6,8 +6,8 @@ GREEN='\033[0;32m'
 
 # Download analytics related files and move them to correct folders
 echo -e "${GREEN}==== Download files with removed analytics ====${NC}"
-wget https://raw.githubusercontent.com/openshift/openshift-docs/master/scripts/ocpdocs/_analytics_other.html -O _templates/_analytics_other.html
-wget https://raw.githubusercontent.com/openshift/openshift-docs/master/scripts/ocpdocs/_footer_other.html.erb -O _templates/_footer_other.html.erb
-wget https://raw.githubusercontent.com/openshift/openshift-docs/master/scripts/ocpdocs/_topnav_other.html -O _templates/_topnav_other.html
-wget https://raw.githubusercontent.com/openshift/openshift-docs/master/scripts/ocpdocs/index-commercial.html -O index-commercial.html
-wget https://raw.githubusercontent.com/openshift/openshift-docs/master/scripts/ocpdocs/search-commercial.html -O search-commercial.html
+wget https://raw.githubusercontent.com/openshift/openshift-docs/main/scripts/ocpdocs/_analytics_other.html -O _templates/_analytics_other.html
+wget https://raw.githubusercontent.com/openshift/openshift-docs/main/scripts/ocpdocs/_footer_other.html.erb -O _templates/_footer_other.html.erb
+wget https://raw.githubusercontent.com/openshift/openshift-docs/main/scripts/ocpdocs/_topnav_other.html -O _templates/_topnav_other.html
+wget https://raw.githubusercontent.com/openshift/openshift-docs/main/scripts/ocpdocs/index-commercial.html -O index-commercial.html
+wget https://raw.githubusercontent.com/openshift/openshift-docs/main/scripts/ocpdocs/search-commercial.html -O search-commercial.html
