59e3055e8c94a1c15ab97deaa6c40d2a930b9944
aireilly <aireilly@redhat.com>
2022-09-05 13:36:18 +0100

    fixes vale script

--- scripts/check-with-vale.sh
+++ scripts/check-with-vale.sh
@@ -14,8 +14,8 @@ if [ -n "${FILES}" ] ;
         echo "==============================================================================================================================="
         echo ""
         #clean out conditional markup
-        sed -i -e 's/ifdef::.*\|ifndef::.*\|ifeval::.*\|endif::.*/ /' "${FILES}"
-        vale "${FILES}" --minAlertLevel=error --glob='*.adoc' --no-exit
+        sed -i -e 's/ifdef::.*\|ifndef::.*\|ifeval::.*\|endif::.*/ /' ${FILES}
+        vale ${FILES} --minAlertLevel=error --glob='*.adoc' --no-exit
         echo ""
         if [ "$TRAVIS" = true ] ; then
             set -x
@@ -27,7 +27,7 @@ if [ -n "${FILES}" ] ;
                 PR_DATA='{"PR": [{"Number": "'"$1"'", "SHA": "'"$2"'"}]',
             fi
             echo "${PR_DATA}" > vale_errors.json
-            ERROR_DATA=$(vale "${FILES}" --minAlertLevel=error --glob='*.adoc' --output=JSON --no-exit)
+            ERROR_DATA=$(vale ${FILES} --minAlertLevel=error --glob='*.adoc' --output=JSON --no-exit)
             echo "${ERROR_DATA:1}" >> vale_errors.json
             LAST_LINE=$(tail -n1 vale_errors.json)
             if echo "$LAST_LINE" | grep -q ',.$'; then
