1c114f212c47a3388bf2cef599b79d83fdc99347
Gaurav Nelson <gaurav.nelson@outlook.com>
2023-08-23 10:31:26 +1000

    Updated the script to track changes in _topic_maps folder

--- autopreview.sh
+++ autopreview.sh
@@ -7,13 +7,46 @@ USERNAME=${TRAVIS_PULL_REQUEST_SLUG::-15}
 COMMIT_HASH="$(git rev-parse @~)"
 mapfile -t FILES_CHANGED < <(git diff --name-only "$COMMIT_HASH")
 
-if [ "$TRAVIS_PULL_REQUEST" != "false" ] ; then #to make sure it only runs on PRs and not all merges
-    if [[ " ${ALLOWED_USERS[*]} " =~ " ${USERNAME} " ]]; then # to make sure it only runs on PRs from @openshift/team-documentation
-        if [ "${TRAVIS_PULL_REQUEST_BRANCH}" != "master" ] ; then # to make sure it does not run for direct master changes
-            if [[ " ${FILES_CHANGED[*]} " = *".adoc"* ]] || [[ " ${FILES_CHANGED[*]} " = *"_topic_map.yml"* ]] || [[ " ${FILES_CHANGED[*]} " = *"_distro_map.yml"* ]] ; then # to make sure this doesn't run for general modifications
-                echo "{\"PR_BRANCH\":\"${TRAVIS_PULL_REQUEST_BRANCH}\",\"BASE_REPO\":\"${TRAVIS_REPO_SLUG}\",\"PR_NUMBER\":\"${TRAVIS_PULL_REQUEST}\",\"USER_NAME\":\"${USERNAME}\",\"BASE_REF\":\"${TRAVIS_BRANCH}\",\"REPO_NAME\":\"${TRAVIS_PULL_REQUEST_SLUG}\"}" > buildset.json
-                curl -H 'Content-Type: application/json' --request POST --data @buildset.json "https://roomy-tungsten-cylinder.glitch.me"
-                echo -e "\\n\\033[0;32m[✓] Sent request for building a preview.\\033[0m"
+if [[ "$USERNAME" == "openshift-cherrypick-robot" ]]; then
+    echo -e "${YELLOW}🤖 PR by openshift-cherrypick-robot. Skipping the preview.${NC}"
+    exit 0
+fi
+
+if [[ "$TRAVIS_PULL_REQUEST" ]]; then
+    # Check if modified files meet the conditions
+    COMMIT_HASH="$(git rev-parse @~)"
+    modified_files=$(git diff --name-only "$COMMIT_HASH")
+    send_request=false
+
+    for file in $modified_files; do
+        if [[ $file == *.adoc || $file == "_topic_map.yml" || $file == "_distro_map.yml" || $file == "_topic_maps/"* ]]; then
+            send_request=true
+            break
+        fi
+    done
+
+    if [ "$send_request" = true ]; then
+        # Build the JSON
+        json_data=$(
+            cat <<EOF
+{
+"PR_BRANCH": "${TRAVIS_PULL_REQUEST_BRANCH}",
+"BASE_REPO": "${TRAVIS_REPO_SLUG}",
+"PR_NUMBER": "${TRAVIS_PULL_REQUEST}",
+"USER_NAME": "${USERNAME}",
+"BASE_REF": "${TRAVIS_BRANCH}",
+"REPO_NAME": "${TRAVIS_PULL_REQUEST_SLUG}",
+"SHA": "${TRAVIS_PULL_REQUEST_SHA}"
+}
+EOF
+        )
+
+        # Send the curl request
+        if response=$(curl -s -X POST -H "Content-Type: application/json" --data "$json_data" https://ocpdocs-preview-receiver.vercel.app/api/buildPreview); then
+            if echo "$response" | jq -e '.message == "Invalid data!"' >/dev/null; then
+                echo -e "${RED}❌😔 Curl request failed: Invalid data!${NC}"
+                echo -e "${YELLOW}$json_data${NC}"
+                exit 1
             else
                 echo -e "\\n\\033[1;33m[!] No .adoc files modified, not building a preview.\\033[0m"
             fi
