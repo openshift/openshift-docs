250cad8b5c70a0220d65fc91697e7416985cf426
aireilly <aireilly@redhat.com>
2023-10-05 09:32:27 +0100

    adding new check distro script
    handle yml distro files
    
    move up netlify
    
    adding _distro_map.yml
    
    updates OCP adoc
    
    updates OCP adoc

--- scripts/get-updated-distros.sh
+++ scripts/get-updated-distros.sh
@@ -4,9 +4,10 @@
 # The list includes any topic maps that are themselves modified, and indirectly modifed topic maps where incldued AsciiDoc files have been updated.
 
 # Get the *.adoc and distro maps files in the pull request
-FILES=$(git diff --name-only HEAD~1 HEAD --diff-filter=d "*.yml" "*.adoc" ':(exclude)_unused_topics/*')
+FILES=$(git diff --name-only HEAD HEAD~$(git rev-list --count --no-merges origin/main..) --diff-filter=d "*.yml" "*.adoc" ':(exclude)_unused_topics/*')
 REPO_PATH=$(git rev-parse --show-toplevel)
-DISTROS=""
+# Init an empty array
+DISTROS=()
 
 # Get the modules in the PR, search for assemblies that include them, and concat with any updated assemblies files
 
@@ -23,7 +24,7 @@ then
     # Subtract $REPO_PATH from path with bash substring replacement
     UPDATED_ASSEMBLIES=${UPDATED_ASSEMBLIES//"$REPO_PATH/"/}
 fi
-# $ASSEMBLIES is the list of modifed assemblies
+# ASSEMBLIES is the list of modifed assemblies
 ASSEMBLIES=$(echo "$FILES" | awk '!/modules\/(.*)\.adoc/')
 # Concatenate both lists and remove dupe entries
 ALL_ASSEMBLIES=$(echo "$UPDATED_ASSEMBLIES $ASSEMBLIES" | tr ' ' '\n' | sort -u)
@@ -34,14 +35,13 @@ for ASSEMBLY in $ALL_ASSEMBLIES; do
     PAGE="File: $(basename "$ASSEMBLY" .adoc)"
     # Don't include the assembly if it is not in a topic map
     if grep -rq "$PAGE" --include "*.yml" _topic_maps ; then
-        DISTROS=$(echo "$(grep -rl "$PAGE" --include "*.yml" _topic_maps)")
+        DISTROS+=("$(grep -rl "$PAGE" --include "*.yml" _topic_maps)")
     fi
 done
 
-# Handle modified topic maps  
+# Handle modified topic maps
 UPDATED_DISTROS=$(echo "$FILES" | awk '/_topic_maps\/(.*)\.yml/')
 # Concat all the updated topic maps
-DISTROS="${DISTROS} ${UPDATED_DISTROS}"
-# Clean up and remove dupes from the list
-DISTROS=$(echo "$DISTROS" | tr ' ' '\n' | sort -u)
-echo "$DISTROS"
+DISTROS+=("${UPDATED_DISTROS}")
+# Clean up and remove duplicates
+echo "${DISTROS[@]}" | tr ' ' '\n' | sort | uniq
