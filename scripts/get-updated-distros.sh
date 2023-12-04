02d8c24e6707a2bbb83c8b362a376f124d0f10f3
Aidan Reilly <74046732+aireilly@users.noreply.github.com>
2023-12-04 00:22:44 +0000

    Revert "fix script to run in  alpine container"

--- scripts/get-updated-distros.sh
+++ scripts/get-updated-distros.sh
@@ -17,7 +17,7 @@ MODULES=$(echo "$FILES" | awk '/modules\/(.*)\.adoc/')
 if [ "${MODULES}" ]
 then
     # $UPDATED_ASSEMBLIES is the list of assemblies that contains changed modules
-    UPDATED_ASSEMBLIES=$(grep -rnwl "$REPO_PATH" --exclude-dir={snippets,modules} -e "$MODULES" -- "*.adoc")
+    UPDATED_ASSEMBLIES=$(grep -rnwl "$REPO_PATH" --include=\*.adoc --exclude-dir={snippets,modules} -e "$MODULES")
     # Exit 0 if there are no modified assemblies
     if [[ -z "${UPDATED_ASSEMBLIES}" ]]
     then
