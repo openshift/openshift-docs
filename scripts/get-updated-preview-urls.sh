4d77a4839c6fcc6d9a4b11305c8edf9b690b8ec9
Aidan Reilly <74046732+aireilly@users.noreply.github.com>
2024-01-31 20:01:21 +0000

    update preview URL script

--- scripts/get-updated-preview-urls.sh
+++ scripts/get-updated-preview-urls.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-# Returns a list of page preview URLs
+# Returns a list of page preview URLs for use with Prow CI jobs
+# To run locally, clean the _preview folder and do a new asciibinder build before running the script
 
 # Check if jq is installed
 hash jq 2>/dev/null || { echo >&2 "Error: jq is not installed"; exit 1; }
@@ -13,7 +14,7 @@ assemblies=()
 pages=()
 files=$(git diff --name-only HEAD~1 HEAD --diff-filter=AMRD "*.adoc" ':(exclude)_unused_topics/*')
 
-# Get the full list of built files
+# Get the full list of HTML build files
 if [ -e "_preview" ]; then
     built_pages=$(find _preview -type f -name "*.html" -printf "%P\n")
 else
@@ -27,14 +28,17 @@ for file in $files; do
     assemblies+=("$found_file")
 done
 
-# Make the html URL slug
+# Make the HTML URL slug
 if [ ${#assemblies[@]} -gt 0 ]; then
     updated_pages=$(echo "${assemblies[@]}" | xargs -n1 basename | sed 's/\.adoc$/.html/' | sort | uniq)
+else
+    # No updated pages, just add default URL
+    pages+=("${preview_url}")
 fi
 
 # Search built_pages for every entry in updated_pages and add to pages array when it is found
 for updated_page in $updated_pages; do
-    # Note also need to sed $pr_branch > "latest"
+    # sed $pr_branch > "latest" to match the Prow build URL
     found_page=$(echo "${built_pages}" | grep "${updated_page}" | sed "s/$pr_branch/latest/")
     pages+=("${preview_url}/${found_page}")
 done
