863c9aa45ea16e8316d4ead499064dfb0082eb24
Aidan Reilly <74046732+aireilly@users.noreply.github.com>
2024-02-07 15:40:21 +0000

    update gitops-docs for prow

--- scripts/check-asciidoctor-build.sh
+++ scripts/check-asciidoctor-build.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-set -e
-
 # ensure asciidoctor is installed
 if ! command -v asciidoctor &>/dev/null ;
 then
@@ -11,7 +9,6 @@ fi
 
 # get the *.adoc modules and assemblies in the pull request
 FILES=$(git diff --name-only HEAD~1 HEAD --diff-filter=d "*.adoc" ':(exclude)_unused_topics/*')
-
 REPO_PATH=$(git rev-parse --show-toplevel)
 
 # get the modules in the PR, search for assemblies that include them, and concat with any updated assemblies files
@@ -21,6 +18,13 @@ check_updated_assemblies () {
     then
         # $UPDATED_ASSEMBLIES is the list of assemblies that contains changed modules
         UPDATED_ASSEMBLIES=$(grep -rnwl "$REPO_PATH" --include=\*.adoc --exclude-dir={snippets,modules} -e "$MODULES")
+        # Exit 0 if there are no modified assemblies
+        if [[ -z "${UPDATED_ASSEMBLIES}" ]]
+        then
+            echo "No updated assemblies found! ✅"
+            exit 0
+        fi
+        update_log
         # subtract $REPO_PATH from path with bash substring replacement
         UPDATED_ASSEMBLIES=${UPDATED_ASSEMBLIES//"$REPO_PATH/"/}
     fi
@@ -29,45 +33,54 @@ check_updated_assemblies () {
     # concatenate both lists and remove dupe entries
     ALL_ASSEMBLIES=$(echo "$UPDATED_ASSEMBLIES $ASSEMBLIES" | tr ' ' '\n' | sort -u)
     # check that assemblies are in a topic_map
-    for ASSEMBLY in $ALL_ASSEMBLIES; do
+    for ASSEMBLY in ${ALL_ASSEMBLIES}; do
         # get the page name to search the topic_map
         # search for files only, not folders
         PAGE="File: $(basename "$ASSEMBLY" .adoc)"
         # don't validate the assembly if it is not in a topic map
         if grep -rq "$PAGE" --include "*.yml" _topic_maps ; then
             # validate the assembly
-            echo "Validating $ASSEMBLY ... 🚨"
-            VALIDATION_ERROR=$(asciidoctor "$ASSEMBLY" -a source-highlighter=rouge -a icons! -o /tmp/out.html -v --failure-level WARN --trace)
+            echo "Validating $ASSEMBLY ..."
+            RED='\033[0;31m'
+            NC='\033[0m'
+            OUTPUT=$(asciidoctor "$ASSEMBLY" -a source-highlighter=rouge -a icons! -o /tmp/out.html -v --failure-level WARN --trace 2>&1)
             # check assemblies and fail if errors are reported
-            if [[ -z "$VALIDATION_ERROR" ]];
+            if [[ $? != 0 ]];
             then
-                echo "No errors found! ✅"
+                echo -e "${RED}$OUTPUT${NC}"
+                ERRORS=true
+            else
+                echo "No errors found for $ASSEMBLY! ✅"
             fi
         else
             echo "$ASSEMBLY is not in a topic_map, skipping validation... 😙"
         fi
     done
+    if [ "$ERRORS" = true ]; then
+        echo "Validation errors found! ❌"
+        exit 1
+    fi
 }
 
 update_log () {
     echo ""
-    echo "************************************************************************"
+    echo "****************************************************************************"
     echo ""
-    echo "Validating all AsciiDoc files that are included in the pull request.  🕵"
-    echo "Other assemblies that include the modifed modules are also validated. 🙀"
-    echo "This might include assemblies that are not in the pull request.       🤬"
-    echo "Validation will fail with FAILED, ERROR, or WARNING messages.         ❌"
-    echo "Correct all reported AsciiDoc errors to pass the validation build.    🤟"
+    echo "Validating all AsciiDoc assemblies that are modifed by the pull request.  🕵"
+    echo "All assemblies that include modifed modules are validated.                🙀"
+    echo "This might include assemblies that are not in the pull request!           🤬"
+    echo "Validation will fail with FAILED, ERROR, or WARNING messages.             ❌"
+    echo "Correct all reported AsciiDoc errors to pass the validation build.        🤟"
     echo ""
-    echo "************************************************************************"
+    echo "****************************************************************************"
     echo ""
 }
 
 # check assemblies and fail if errors are reported
 if [ -n "${FILES}" ] ;
 then
-    update_log
     check_updated_assemblies
 else
     echo "No modified AsciiDoc files found! 🥳"
+    exit 0
 fi
