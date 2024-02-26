6be5b06ffaf5f5d0bde0236101768af2b093615d
Aidan Reilly <74046732+aireilly@users.noreply.github.com>
2024-02-26 18:15:18 +0000

    refactor vale-review

--- scripts/vale-review.sh
+++ scripts/vale-review.sh
@@ -10,9 +10,9 @@ COMMIT_ID=$(git log -n 1 --pretty=format:"%H")
 PULL_NUMBER=$(curl -s "https://api.github.com/search/issues?q=$COMMIT_ID" | jq '.items[0].number')
 REPO_OWNER="openshift"
 REPO_NAME="openshift-docs"
+FILES=$(git diff --name-only HEAD~1 HEAD --diff-filter=d "*.adoc" ':(exclude)_unused_topics/*')
 
-# Build curl command for Github API review comment.
-function post_variable_metadata {
+function send_github_review {
 
     LINE_NUMBER=$3
     BODY=$1
@@ -22,40 +22,36 @@ function post_variable_metadata {
 
 }
 
-# Get update files - this only gets files updated  in the last commit. For unsquashed commits, it will only run on last commit
-# This might be helpful to find all commits since head if the needs arises: https://stackoverflow.com/questions/7693249/how-to-list-commits-since-certain-commit
-FILES=$(git diff --name-only HEAD~1 HEAD --diff-filter=d "*.adoc" ':(exclude)_unused_topics/*')
-
 # Compares body, path, and line values for Vale alerts vs already existing comments on PR. If values match, comment already exists, remove from temp json files.
 function check_if_existing_comment {
 
     echo "Checking if existing comment..."
 
-    local vale_file="$1"
-    local pull_comments_file="$2"
+    local vale_review="$1"
+    local pull_comments="$2"
 
     # Iterate through Vale alert JSON
-    jq -c '.[]' "$vale_file" | while IFS= read -r object; do
-    BODY_VALE=$(echo "$object" | jq -r '.body')
-    FILENAME_VALE=$(echo "$object" | jq -r '.path')
-    LINE_NUMBER_VALE=$(echo "$object" | jq -r '.line')
+    jq -c '.[]' "$vale_review" | while IFS= read -r json_object; do
+    BODY_VALE=$(echo "$json_object" | jq -r '.body')
+    FILENAME_VALE=$(echo "$json_object" | jq -r '.path')
+    LINE_NUMBER_VALE=$(echo "$json_object" | jq -r '.line')
 
         # Iterate through pull comment JSON to see if a comment exists
-        jq -c '.[]' "$pull_comments_file" | while IFS= read -r object2; do
-            BODY_COMMENT=$(echo "$object2" | jq -r '.body')
-            FILENAME_COMMENT=$(echo "$object2" | jq -r '.path')
-            LINE_NUMBER_COMMENT=$(echo "$object2" | jq -r '.line')
+        jq -c '.[]' "$pull_comments" | while IFS= read -r json_object2; do
+            BODY_COMMENT=$(echo "$json_object2" | jq -r '.body')
+            FILENAME_COMMENT=$(echo "$json_object2" | jq -r '.path')
+            LINE_NUMBER_COMMENT=$(echo "$json_object2" | jq -r '.line')
 
             # If a there's a comment that matches the Vale body, line and path, remove it from both files
             if [ "$BODY_VALE" == "$BODY_COMMENT" ] && [ "$FILENAME_VALE" == "$FILENAME_COMMENT" ] && [ "$LINE_NUMBER_VALE" == "$LINE_NUMBER_COMMENT" ]; then
-                echo "Found existing comment for $object"
+                echo "Found existing comment for $json_object"
                 echo "Removing alert because review comment already exists... "
                 
-                jq --argjson obj "$object" '. | del(.[index($obj)])' "$vale_file" > processing.json
-                jq --argjson obj "$object2" '. | del(.[index($obj)])' "$pull_comments_file" > processing_comments.json
+                # jq --argjson obj "$json_object" '. | del(.[index($obj)])' "$vale_review" > processing.json
+                # jq --argjson obj "$json_object2" '. | del(.[index($obj)])' "$pull_comments" > processing_comments.json
 
-                mv processing.json "$vale_file"
-                mv processing_comments.json "$pull_comments_file"
+                # mv processing.json "$vale_file"
+                # mv processing_comments.json "$pull_comments_file"
 
                 break
 
@@ -67,33 +63,28 @@ function check_if_existing_comment {
 
 }
 
+function get_pr_comments {
+    curl -L -H "Accept: application/vnd.github+json" -H "Authorization: Bearer $GITHUB_AUTH_TOKEN" -H "X-GitHub-Api-Version: 2022-11-28" https://api.github.com/repos/$REPO_OWNER/$REPO_NAME/pulls/$PULL_NUMBER/comments
+}
+
 # Run vale with the custom template on updated files
-for FILE in ${FILES}; 
-do
-    valeOutput=$(vale --minAlertLevel=error --output=.vale/templates/bot-comment-output.tmpl $FILE)
-    
-    # Converts JSON stream to array
-    finalJson=$(jq -s '.' <<< "$valeOutput")
-        
-    # Moved JSON to temp file for further parsing to avoid issues during development, but might not be neccessary(?)
-    echo "$finalJson" > vale_output_temp.json
+for FILE in ${FILES}; do
 
-    #Get comments in PR
-    curl -L -H "Accept: application/vnd.github+json" -H "Authorization: Bearer $GITHUB_AUTH_TOKEN" -H "X-GitHub-Api-Version: 2022-11-28" https://api.github.com/repos/$REPO_OWNER/$REPO_NAME/pulls/$PULL_NUMBER/comments > pull_comments_temp.json
+    vale_review_json=$(vale --minAlertLevel=error --output=.vale/templates/bot-comment-output.tmpl | jq .)
 
-    check_if_existing_comment "vale_output_temp.json" "pull_comments_temp.json"
+    pull_comments_json=$(get_pr_comments)
+
+    check_if_existing_comment "$vale_review_json" "$pull_comments_json"
 
     # Following logic checks if the line number is a part of the git diff. If it's not part of the diff it will be discarded.
     # We only want to check new/modified content, plus the GitHub API only accepts comments within the diff for the review comments endpoint.
     #echo "Checking line number is within the diff..."
 
-    # Iterate through the JSON objects
-    jq -c '.[]' vale_output_temp.json | while IFS= read -r object; do
-        #echo "$object"
-        BODY=$(echo "$object" | jq -r '.body')
-        FILENAME=$(echo "$object" | jq -r '.path')
-        LINE_NUMBER=$(echo "$object" | jq -r '.line')
-        
+    # Iterate through the vale json review objects
+    jq -c '.[]' "$vale_review_json" | while IFS= read -r json_object; do
+        BODY=$(echo "$json_object" | jq -r '.body')
+        FILENAME=$(echo "$json_object" | jq -r '.path')
+        LINE_NUMBER=$(echo "$json_object" | jq -r '.line')
         # Check the unified diff for the alert and file
         FULL_DIFF=$(git diff --unified=0 --stat --diff-filter=AM HEAD~1 HEAD "${FILENAME}" ':(exclude)_unused_topics/*')
         
@@ -125,9 +116,9 @@ do
                 # Check if the LINE_NUMBER falls within the range (diff_start_line) to (diff_start_line + added_lines)
                 if (( LINE_NUMBER >= diff_start_line && LINE_NUMBER <= diff_start_line + added_lines )); then
                     
-                    post_variable_metadata "$BODY" "$FILENAME" "$LINE_NUMBER"
+                    send_github_review "$BODY" "$FILENAME" "$LINE_NUMBER"
                     
-                    break  # Exit the loop since the alert is within the diff, move on to the next JSON object
+                    break  # Exit the loop since the alert is within the diff, move on to the next JSON json_object
                 fi
             fi
                 
