d09c1b0e8c2017e2c18ed383ab55426ea1cee00a
Kathryn Alexander <kalexand@redhat.com>
2024-06-27 13:05:51 -0400

    s/access.redhat/docs.redhat in build_for_portal.py

--- build_for_portal.py
+++ build_for_portal.py
@@ -25,7 +25,7 @@ cli.init_logging(False, True)
 
 list_of_errors = []
 CLONE_DIR = "."
-BASE_PORTAL_URL = "https://access.redhat.com/documentation/en-us/"
+BASE_PORTAL_URL = "https://docs.redhat.com/en-us/documentation"
 # ID_RE = re.compile("^\[(?:\[|id=\'|#)(.*?)(\'?,.*?)?(?:\]|\')?\]", re.M | re.DOTALL)
 ID_RE = re.compile(
     "^\[(?:\[|id='|#|id=\")(.*?)('?,.*?)?(?:\]|'|\")?\]", re.M | re.DOTALL
