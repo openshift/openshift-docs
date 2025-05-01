23254573d5c1ddff5502d54368b0ee87bef52188
Gabriel McGoldrick <gmcgoldr@redhat.com>
2025-05-01 22:33:50 +0100

    update devcontainer with yq, fix errors

--- scripts/package.sh
+++ scripts/package.sh
@@ -13,14 +13,14 @@ USE_LOCAL="${USE_LOCAL:-false}"
 ## CLONE REPO
 echo "---> Cloning docs from $BRANCH branch in $REPO"
 # Clone OpenShift Docs into current directory
-git clone --branch $BRANCH --depth 1 $REPO .docs_source
+git clone --branch $BRANCH --depth 1 --no-single-branch $REPO .docs_source
 
 cd .docs_source
-# it's necessary to enforce a * ref so that all branches are referenced
-sed -i 's%fetch = +refs.*%fetch = +refs/heads/*:refs/remotes/origin/*%' .git/config
-git fetch --all --quiet
+
+
 for remote in $(cat _distro_map.yml | yq eval ".*.branches | keys | .[]" - | sort | uniq)
 do
+    git fetch origin $remote
     echo "checkout $remote"
     git checkout $remote 2>/dev/null || git checkout --force --track remotes/origin/$remote
 done
